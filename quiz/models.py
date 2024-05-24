c41e2ae288363b3fd331a6fe3a9fe6df6bf6c749
Joha Wilson <johawilsonatu@icloud.com>
2024-05-25 00:18:32 +0100

    adjusted quiz model return value and made migrations

--- quiz/models.py
+++ quiz/models.py
@@ -8,4 +8,4 @@ class Quiz(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.score
+        return f"{self.user.username} - {self.score}"
