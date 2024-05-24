d7e1d3130a19a0da76fe41de9a464c00fbbd4839
Joha Wilson <johawilsonatu@icloud.com>
2024-05-25 00:08:06 +0100

    created quiz model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Quiz(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField(null=True, blank=True,)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.score
