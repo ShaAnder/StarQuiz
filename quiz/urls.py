53c98e808f40d11ca508f5ef600cf34ddf22d0cf
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-05-25 22:07:01 +0200

    Create QuizForm, integrate it in vi ews and correct view logic

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('quiz/', views.start_quiz, name='start_quiz'),
-    path('submit_quiz/', views.submit_quiz, name='submit_quiz'),
+    path('', views.quiz, name='quiz'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,29 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .forms import QuizForm
 from frontend.models import Trivia
 from quiz.models import Quiz
 
 @login_required
-def start_quiz(request):
-    questions = Trivia.objects.all()
-    return render(request, 'quiz/quiz.html', {questions:questions})
+def quiz(request):
+    if request.method == 'POST':
+        form = QuizForm(request.POST)
+        if form.is_valid():
+            score = calculate_score(form.cleaned_data)
+            save_quiz(request.user, score)
+            return render(request, 'quiz_result.html', {'score': score})
+    else:
+        form = QuizForm()
+    return render(request, 'quiz.html', {'form': form})
+
+def calculate_score(answers):
+    score = 0
+    for question_id, user_answer in answers.items():
+        question = get_object_or_404(Trivia, id=question_id.split('_')[1])
+        if user_answer == question.answer:
+            score += 1
+    return score
+
+def save_quiz(user, score):
+    quiz = Quiz(user=user, score=score)
+    quiz.save()
 
 @login_required
-def submit_quiz(request):
-    if request.method == 'POST':
-        score = 0
-        questions = Trivia.objects.all()
-        for question in questions:
-            user_answer = request.POST.get(f'question_{question.id}')
-            if user_answer == question_answer:
-                score += 1
-        quiz = Quiz(user=request.user, score=score)
-        quiz.save()
-        return render(request, 'quiz/quiz_result.html', {'score': score})
-    return render(request, 'quiz/quiz.html')
-
-
 def leaderboard(request):
-    quizzes = Quiz.objects.order_by('-score')[-10]
-    return render(request, 'quiz/leaderboard.html', {'quizzes':quizzes})
-
+    quizzes = Quiz.objects.order_by('-score')[:10]
+    return render(request, 'leaderboard.html', {'quizzes': quizzes})
