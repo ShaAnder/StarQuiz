39f81b76e4ac96d89bb568861787a1dd6f21e753
Joha Wilson <johawilsonatu@icloud.com>
2024-05-26 23:19:10 +0100

    styles index.html begin button, and styles quiz.html

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 
 class QuizForm(forms.Form):
-    answer = forms.CharField(label='Your Answer', max_length=100)
+    answer = forms.CharField(label='', max_length=100)
 
     def __init__(self, *args, **kwargs):
         question = kwargs.pop('question', None)
         super(QuizForm, self).__init__(*args, **kwargs)
-        if question:
-            self.fields['answer'].label = question.question
+        for field in self.fields:
+            self.fields[field].widget.attrs['placeholder'] = 'Your Answer'
+            self.fields[field].widget.attrs['class'] = 'p-5 max-w-lg w-1/3 rounded'
+        
