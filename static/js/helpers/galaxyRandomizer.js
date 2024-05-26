50a1b435ece276c86034681a11df4d10a714d77d
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-05-27 01:31:36 +0200

    Change js function for planets and style galaxy page

--- static/js/helpers/galaxyRandomizer.js
+++ static/js/helpers/galaxyRandomizer.js
@@ -1,15 +1,17 @@
-   
-    document.addEventListener("DOMContentLoaded", function() {
-        var container = document.querySelector('.galaxy-container');
-        var planets = document.querySelectorAll('.planet-item');
-        var containerWidth = container.clientWidth;
-        var containerHeight = container.clientHeight;
-    
-        planets.forEach(function(planet) {
-            var randomX = Math.random() * (containerWidth - 200);
-            var randomY = Math.random() * (containerHeight - 200);
-            planet.style.left = randomX + 'px';
-            planet.style.top = randomY + 'px';
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    var container = document.querySelector('.galaxy-container');
+    var planets = document.querySelectorAll('.planet-item');
+    var containerWidth = container.clientWidth;
+    var containerHeight = container.clientHeight;
+    var numPlanets = planets.length;
+    var radius = Math.min(containerWidth, containerHeight) * 0.3; // Adjust the radius to make the circle smaller
+    var angleIncrement = (2 * Math.PI) / numPlanets;
+
+    planets.forEach(function(planet, index) {
+        var angle = index * angleIncrement;
+        var x = containerWidth / 2 + radius * Math.cos(angle) - planet.offsetWidth / 2;
+        var y = containerHeight / 2 + radius * Math.sin(angle) - planet.offsetHeight / 2;
+        planet.style.left = x + 'px';
+        planet.style.top = y + 'px';
     });
-    
+});
