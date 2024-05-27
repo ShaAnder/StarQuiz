49e1079940aaed7a4350da740e1b0fec3c5fb099
Joha Wilson <johawilsonatu@icloud.com>
2024-05-27 08:55:30 +0100

    collect static for deployment

---
+++ staticfiles/js/galaxy.342007835e02.js
@@ -0,0 +1,19 @@
+const stars = 500;
+
+for (let i = 0; i < stars; i++) {
+    let star = document.createElement("div");
+    star.className = "stars";
+    let x = Math.floor(Math.random() * window.innerWidth);
+    let y = Math.floor(Math.random() * window.innerHeight);
+    star.style.top = y + "px";
+    star.style.left = x + "px";
+    document.body.append(star);
+}
+
+function randomPosition() {
+    let x = window.innerWidth;
+    let y = window.innerHeight;
+    let randomX = Math.floor(Math.random() * x);
+    let randomY = Math.floor(Math.random() * y);
+    return [randomY, randomX];
+}
