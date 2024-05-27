60936e07685121740957f49459ea4e2c10f06b65
Joha Wilson <johawilsonatu@icloud.com>
2024-05-27 15:04:59 +0100

    updated allowed host

--- backend_main/settings.py
+++ backend_main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://jedi-path-quizventure.onrender.com']
+ALLOWED_HOSTS = ['jedi-path-quizventure.onrender.com']
 
 
 # Application definition
